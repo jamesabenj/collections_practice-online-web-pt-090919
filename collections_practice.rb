615e8058132ebc32e1ec80ce1af048bd76093ecc
James Benjamin <james.abenj@gmail.com>
2019-09-21 03:23:54 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,53 @@
+def sort_array_asc(array_of_ints)
+  array_of_ints.sort
+end
+
+def sort_array_desc(array_of_ints)
+  array_of_ints.sort.reverse
+end
+    
+def sort_array_char_count(array_of_strings)
+  array_of_strings.sort{|a, b| a.length <=> b.length}
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse 
+end
+
+def kesha_maker(array)
+ array.each do |string|
+    string[2] = "$"
+  end
+end
+
+def find_a(array)
+  array.select{|i| i.start_with?("a")}
+end
+    
+def sum_array(array)
+  new_array = []
+  array.each do |i|
+    if i.is_an_int
+      new_array << i
+    else
+      next
+    end
+  end
+new_array.inject(:+)
+  end
+end
+
+
+
+
+
+    
+    
+  
+    
 
